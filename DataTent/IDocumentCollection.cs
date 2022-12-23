fc0388b784dda4da713784f397210aecae4ab042
J Wynia <j@wynia.org>
2022-12-23 14:08:39 -0600

    Normalize names

--- DataTent/IDocumentCollection.cs
+++ DataTent/IDocumentCollection.cs
@@ -8,9 +8,9 @@ namespace DataTent
 	{
 		IQueryable<T> AsQueryable();
 		IEnumerable<T> Find(Predicate<T> query);
-		bool InsertOne(T item, string uniqueId);
-		bool ReplaceOne(T item, string uniqueId);
-		bool DeleteOne(string uniqueId);
+		bool Insert(T item, string uniqueId);
+		bool Replace(T item, string uniqueId);
+		bool Delete(string uniqueId);
 		int Count { get; }
 		void Reload();
         
